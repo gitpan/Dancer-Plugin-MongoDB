18364ed67604f0cddb6cd97cc6e5fb6c84ae965d
Tyler Hardison <tyler@seraph-net.net>
2014-04-21 18:31:38 +0000

    Import of THARDISON/Dancer-Plugin-MongoDB-0.35 from CPAN.
    gitpan-cpan-distribution: Dancer-Plugin-MongoDB
    gitpan-cpan-version:      0.35
    gitpan-cpan-path:         THARDISON/Dancer-Plugin-MongoDB-0.35.tar.gz
    gitpan-cpan-author:       THARDISON
    gitpan-cpan-maturity:     released

--- lib/Dancer/Plugin/MongoDB.pm
+++ lib/Dancer/Plugin/MongoDB.pm
@@ -33,7 +33,7 @@ is not the case.
 
 =cut
 
-our $VERSION = 0.34;
+our $VERSION = 0.35;
 
 my $settings = undef;
 my $conn = undef;
@@ -290,7 +290,7 @@ __END__
 
 =head1 VERSION
 
-version 0.34
+version 0.35
 
 =head1 SYNOPSIS
 
